b7ee0425b0e126997cedf4ea7235b28cb2100c2f
charlie-rudenstal <charlie4@gmail.com>
2012-10-03 20:45:56 +0200

    Fixed bugg with coordinate 0 on moving

--- public/js/FlexEditor/FlexEditor.js
+++ public/js/FlexEditor/FlexEditor.js
@@ -487,55 +487,55 @@ function toRelative(fromRect) {
 };
 
 Button.prototype.x = function(value, positionType) {
-	if(value) 
-		if(positionType == "absolute") 
-			this.rect.x = value / 800 * 100;
-		else 
-			this.rect.x = value;
-	else 
+	if(value == null) 
 		if(positionType == "absolute") 
 			return this.rect.x / 100 * 800;
 		else 
 			return this.rect.x;
+	else 
+		if(positionType == "absolute") 
+			this.rect.x = value / 800 * 100;
+		else 
+			this.rect.x = value;
 };
 
 Button.prototype.y = function(value, positionType) {
-	if(value) 
-		if(positionType == "absolute") 
-			this.rect.y = value / 800 * 100;
-		else 
-			this.rect.y = value;
-	else 
+	if(value == null) 
 		if(positionType == "absolute") 
 			return this.rect.y / 100 * 800;
 		else 
 			return this.rect.y;
+	else 
+		if(positionType == "absolute") 
+			this.rect.y = value / 800 * 100;
+		else 
+			this.rect.y = value;
 };
 
 Button.prototype.width = function(value, positionType) {
-	if(value) 
-		if(positionType == "absolute") 
-			this.rect.width = value / 800 * 100;
-		else 
-			this.rect.width = value;
-	else 
+	if(value == null) 
 		if(positionType == "absolute") 
 			return this.rect.width / 100 * 800;
 		else 
 			return this.rect.width;
+	else 
+		if(positionType == "absolute") 
+			this.rect.width = value / 800 * 100;
+		else 
+			this.rect.width = value;
 };
 
 Button.prototype.height = function(value, positionType) {
-	if(value) 
-		if(positionType == "absolute") 
-			this.rect.height = value / 800 * 100;
-		else 
-			this.rect.height = value;
-	else 
+	if(value == null) 
 		if(positionType == "absolute") 
 			return this.rect.height / 100 * 800;
 		else 
 			return this.rect.height;
+	else 
+		if(positionType == "absolute") 
+			this.rect.height = value / 800 * 100;
+		else 
+			this.rect.height = value;
 };
 
 function Modal(options) {
