3a4eb4f454c944c3b99e66aa659ad52ebe66d7f2
charlie <charlie4@gmail.com>
2012-10-21 15:51:00 +0200

    Added welcome message with setup for new clients

--- app.js
+++ app.js
@@ -18,18 +18,27 @@ server.listen(app.get('port'), function(){
 });
 
 var connectionCount = 0;
+var currentExportedData;
+var currentBackground;
 
 // Socket for real time preview in iphone/device
 io.sockets.on('connection', function (socket) {
  	connectionCount++;
+ 	
+ 	// Welcome status to new clients
  	io.sockets.emit('connection-count', connectionCount);
-	
+	if(currentExportedData) io.sockets.emit('change', currentExportedData);
+	if(currentBackground) io.sockets.emit('background', currentBackground);
+
+
 	socket.on('change', function (exportedData) {
-		io.sockets.emit('change', exportedData)
+		io.sockets.emit('change', exportedData);
+		currentExportedData = exportedData;
  	});
 
  	socket.on('background', function (hexColor) {
-		io.sockets.emit('background', hexColor)
+		io.sockets.emit('background', hexColor);
+		currentBackground = hexColor;
  	});
 
  	socket.on('disconnect', function() {
